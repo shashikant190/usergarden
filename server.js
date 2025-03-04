5ae2b97092a6c767a1699218c11f5c21d9f132a8
Shashikant Maurya <mshashikant3600@gmail.com>
2025-03-04 11:35:47 +0530

    Version 3.0

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 // Initialize Supabase
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_ANON_KEY
 );
 
 app.use(cors());
